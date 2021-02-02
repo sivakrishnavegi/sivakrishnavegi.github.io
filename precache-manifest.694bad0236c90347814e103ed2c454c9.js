d95f37235f57b8c753085528fd778e57b69d20df
siva <sivakrishnavegi@yahoo.com>
2021-02-02 20:26:42 +0530

    added a link

---
+++ precache-manifest.694bad0236c90347814e103ed2c454c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b33dc6f367cd15da49b81c4dc33d921",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62e058d713c5f8d08a0d",
+    "url": "/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "bceb12807aa11fe2955f",
+    "url": "/static/css/main.8e09fb7e.chunk.css"
+  },
+  {
+    "revision": "62e058d713c5f8d08a0d",
+    "url": "/static/js/2.1176f3d1.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.1176f3d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bceb12807aa11fe2955f",
+    "url": "/static/js/main.1214ffd9.chunk.js"
+  },
+  {
+    "revision": "51b1263d09889fed3508",
+    "url": "/static/js/runtime-main.07ad49ca.js"
+  }
+]);

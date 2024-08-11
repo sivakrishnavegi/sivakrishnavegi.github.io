48ce06ab706e0bff36c003b40e4bc0a629f66e83
sivakrishnavegi <sivakrishnavegi@yahoo.com>
2024-08-11 13:10:17 +0530

    adde nav

--- core/containers/home/index.tsx
+++ core/containers/home/index.tsx
@@ -1,14 +1,22 @@
-import HomePageLayout from '@/core/layout/homepage'
-import React from 'react'
+import HeroBanner from "@/core/components/HeroBanner";
+import { FloatingNav } from "@/core/components/ui/FloatingNavbar";
+import HomePageLayout from "@/core/layout/homepage";
+import { navItems } from "@/data";
+import React from "react";
 
 const HomePageContainer = () => {
   return (
     <div>
-        <HomePageLayout>
-            <div>homepage</div>
-        </HomePageLayout>
+      <HomePageLayout>
+        <div className="relative flex justify-center items-center bg-black-100 flex-col overflow-hidden mx-auto sm:px-10 px-5">
+          <div className="max-w-7xl w-full">
+            <FloatingNav navItems={navItems} />
+            <HeroBanner />
+          </div>
+        </div>
+      </HomePageLayout>
     </div>
-  )
-}
+  );
+};
 
-export default HomePageContainer
+export default HomePageContainer;
--- core/providers/RootProvider.tsx
+++ core/providers/RootProvider.tsx
@@ -1,13 +1,18 @@
-'use client';
+"use client";
 
 import { ThemeProvider } from "./NextThemeProvider";
 
 export const RootProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-            <ThemeProvider attribute='class' defaultTheme='dark'>
-              <div className='min-h-screen'>{children}</div>
-            </ThemeProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <div className="min-h-screen">{children}</div>
+      </ThemeProvider>
     </div>
   );
 };

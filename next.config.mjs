9841fa9e8926660f28313a62dcac412add02acfb
sivakrishnavegi <sivakrishnavegi@yahoo.com>
2025-01-29 17:04:54 +0530

    added data

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-          'via.placeholder.com',
-        ],
-      },
+  output: "export",
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  //   distDir: 'dist',
+  images: {
+    domains: ["via.placeholder.com"],
+  },
 };
 
 export default nextConfig;
